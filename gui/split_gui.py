6f16a7d45ddb1ca79eb680c01566f3b470feb7a5
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-06-04 15:48:08 +0530

    Improve error handling and logs to PDF split GUI

--- gui/split_gui.py
+++ gui/split_gui.py
@@ -3,6 +3,7 @@
 import os
 from tkinter import filedialog, simpledialog, messagebox
 from config.config import setup_logger
+from core.error_handler import handle_exception
 from gui.error_handler_gui import show_message
 from core.pdf_splitter import split_pdf
 
@@ -15,46 +16,55 @@ def split_pdf_gui():
     Invokes file dialogs for input/output and delegates logic to core.
     """
 
-    file_path = filedialog.askopenfilename(
-        title="Select PDF to split",
-        filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.pdf")],
-        defaultextension="*.pdf",
-    )
-
-    if not file_path:
-        messagebox.showwarning(
-            title="No file selected", message="Please select a PDF to split."
+    logger.info("Split PDF operation started")
+    try:
+        file_path = filedialog.askopenfilename(
+            title="Select PDF to split",
+            filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.pdf")],
+            defaultextension="*.pdf",
         )
-        logger.warning("No PDF file selected.")
-        return
-    page_range_input = simpledialog.askstring(
-        title="Page ranges", prompt="Enter the page ranges (e.g., 1-3,5,7-9):"
-    )
 
-    if not page_range_input or not (page_range_input := page_range_input.strip()):
-        messagebox.showwarning(
-            title="Invalid Input", message="Please enter a valid page range."
+        if not file_path:
+            messagebox.showwarning(
+                title="No file selected", message="Please select a PDF to split."
+            )
+            logger.warning("Splitting failed - No PDF file selected.")
+            return
+        page_range_input = simpledialog.askstring(
+            title="Page ranges", prompt="Enter the page ranges (e.g., 1-3,5,7-9):"
         )
-        logger.warning("Page range not provided or empty.")
-        return
 
-    output_dir = filedialog.askdirectory(title="Select folder to save the split files")
+        if not page_range_input or not (page_range_input := page_range_input.strip()):
+            messagebox.showwarning(
+                title="Invalid Input", message="Please enter a valid page range."
+            )
+            logger.warning("Splitting failed - Page range not provided or empty.")
+            return
 
-    if not output_dir:
-        messagebox.showwarning(
-            title="No Folder selected", message="Output directory not selected."
+        output_dir = filedialog.askdirectory(
+            title="Select folder to save the split files"
         )
-        logger.warning("No output folder selected.")
-        return
 
-    result = split_pdf(file_path, page_range_input, output_dir)
-    if result.error_type == "info":
-        logger.info(f"{result.message}")
-        if result.data and "files" in result.data:
-            for f in result.data["files"]:
-                logger.info(f"Created: {os.path.join(output_dir,f)}")
-        logger.info(f"Split operation finished with message: {result.message}")
-    else:
-        logger.warning(f"{result.title}: {result.message}")
+        if not output_dir:
+            messagebox.showwarning(
+                title="No Folder selected", message="Output directory not selected."
+            )
+            logger.warning("Splitting failed - No output folder selected.")
+            return
 
-    show_message(result)
+        result = split_pdf(file_path, page_range_input, output_dir)
+        if result.error_type == "info":
+            logger.info(f"{result.message}")
+            if result.data and "files" in result.data:
+                for f in result.data["files"]:
+                    logger.info(f"Created: {os.path.join(output_dir,f)}")
+            logger.info(f"Split operation finished with message: {result.message}")
+        else:
+            logger.warning(f"{result.title}: {result.message}")
+
+        show_message(result)
+
+    except Exception as exc:
+        error_msg = handle_exception(exc, context="Splitting PDF")
+        show_message(error_msg)
+        logger.error(f"Exception during splitting PDF: {exc}")
