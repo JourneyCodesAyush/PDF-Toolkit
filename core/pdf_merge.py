b32967fb08b76145a77d16a484255dd05291e276
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-06-13 14:06:00 +0530

    Add PDF validity check for each file before merging

--- core/pdf_merge.py
+++ core/pdf_merge.py
@@ -3,6 +3,7 @@
 from PyPDF2 import PdfMerger
 from core import error_handler
 from core.result import Result
+from core.utils import validate_pdf_file
 
 
 def mergePDF(input_file_path: list[str], output_file_path: str) -> Result:
@@ -21,14 +22,13 @@ def mergePDF(input_file_path: list[str], output_file_path: str) -> Result:
         return Result(
             success=False, title="No files", message="No input files selected."
         )
-        # return success_failure("Failure", message="No input files selected")
 
     merger = PdfMerger()
     try:
         for pdf in input_file_path:
-            if not pdf.lower().endswith(".pdf"):
-                # success_failure("Failure", message=ValueError)
-                raise ValueError(f"Invalid file format (not a PDF): {pdf}")
+            is_valid, error_message = validate_pdf_file(path=pdf)
+            if not is_valid:
+                raise ValueError(f"{error_message}")
             merger.append(pdf)
         merger.write(output_file_path)
         return Result(
@@ -37,10 +37,9 @@ def mergePDF(input_file_path: list[str], output_file_path: str) -> Result:
             message="PDFs merged successfully!",
             error_type="info",
         )
-        # return success_failure("Success", "PDFs merged successfully!")
+
     except Exception as e:
         return error_handler.handle_exception(exc=e, context="merging PDFs")
-        # return success_failure("Failure", message=str(e))
 
     finally:
         merger.close()
