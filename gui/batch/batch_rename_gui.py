7a59b3aa596e07a854d85fd90603f10d86d81975
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-06-24 17:30:30 +0530

    feat(gui/batch): add GUI handler for batch rename PDF operation

---
+++ gui/batch/batch_rename_gui.py
@@ -0,0 +1,87 @@
+# Batch PDF rename GUI
+
+
+import os
+from config.config import setup_logger
+from core.error_handler import handle_exception
+from gui.error_handler_gui import show_message
+from core.batch.batch_rename import batch_rename_pdfs
+from tkinter import messagebox, filedialog, simpledialog
+
+logger = setup_logger(__name__)
+
+
+def batch_rename_pdf_gui():
+    """
+    GUI handler to rename a PDF file.
+
+    Opens dialogs for the user to select the original PDF,
+    choose the output directory, and enter a new file name.
+    Calls the core rename function and displays the result.
+
+    Returns:
+        None: This function performs GUI interactions and shows messages,
+              no return value is needed.
+    """
+    logger.info("Rename PDF operation started")
+
+    try:
+        input_dir = filedialog.askdirectory(
+            title="Select the folder you wish to rename the PDFs of..."
+        )
+
+        if not input_dir:
+            messagebox.showwarning(
+                title="Warning",
+                message="Please select a folder whose PDF files you wish to rename.",
+            )
+            logger.warning(f"Batch renaming failed - No folder selected")
+            return
+
+        output_path_yes_no = messagebox.askyesno(
+            title="Output folder",
+            message="Do you want to save the merged folder elsewhere?",
+        )
+
+        if output_path_yes_no:
+            output_dir = filedialog.askdirectory(
+                title="Select a directory to save the renamed files"
+            )
+        else:
+            output_dir = input_dir
+
+
+        new_file_name = simpledialog.askstring(
+            title="Rename PDF",
+            prompt="Enter the new file name (without .pdf extension)",
+        )
+
+        if not new_file_name or not new_file_name.strip():
+            messagebox.showwarning(
+                title="Warning", message="Please enter a valid file name."
+            )
+            logger.warning(f"Renaming failed - New file name NOT selected")
+            return
+
+        new_file_name = new_file_name.strip()
+
+        logger.info(
+            f"Renaming files inside: {input_dir} -> {os.path.join(output_dir, new_file_name)}"
+        )
+        result = batch_rename_pdfs(input_dir, output_dir, new_file_name)
+
+        if result.success:
+            logger.info(f"Renaming successful: {result.message}")
+        else:
+            logger.warning(
+                f"Rename operation returned failure message: {result.message}"
+            )
+
+        show_message(result)
+
+    except Exception as exc:
+        error_msg = handle_exception(exc, context="Renaming PDF")
+        show_message(error_msg)
+        logger.error(
+            f"Renaming PDF failed due to an unexpected error. We are sorry for your inconvenience!"
+        )
