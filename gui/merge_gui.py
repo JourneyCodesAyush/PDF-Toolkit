68b11b8e360b2dd9c1c049fbb4b3077ca8062702
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-08-27 12:54:23 +0530

    feat(gui/merge-gui): open last used folder for PDF merge if user opted to save preferences

--- gui/merge_gui.py
+++ gui/merge_gui.py
@@ -1,8 +1,10 @@
 # PDF merging GUI part here
 
+from pathlib import Path
 from tkinter import filedialog, messagebox
 
 from config.config import setup_logger
+from config.preferences_manager import get_preferences, set_preferences
 from core.error_handler import handle_exception
 from core.pdf_merge import merge_pdf
 from gui.error_handler_gui import show_message
@@ -25,7 +27,14 @@ def merge_pdf_gui(root) -> None:
     logger.info("Merge PDF operation started")
 
     try:
+        prefs = get_preferences()
+        initial_directory = Path.home()
+        if prefs.get("save_preferences") and prefs.get("last_merged_input_file"):
+            initial_directory = prefs["last_merged_input_file"]
+            initial_directory = Path(str(initial_directory))
+
         input_files = filedialog.askopenfilenames(
+            initialdir=str(initial_directory),
             title="Select PDFs to merge",
             filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.pdf")],
             defaultextension="*.pdf",
@@ -60,6 +69,14 @@ def merge_pdf_gui(root) -> None:
                     f"Merged files: { ', '.join([input_file for input_file in input_files])} to {save_file_path}"
                 )
                 logger.info(f"Merging Successful: {result.message}")
+
+                # Update the preferences if user opted for it
+
+                if prefs.get("save_preferences"):
+                    set_preferences(
+                        last_merged_input_file=str(Path(input_files[-1]).parent)
+                    )
+
             else:
                 logger.warning(f"Merge returned failure message: {result.message}")
 
