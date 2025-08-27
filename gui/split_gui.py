4aa6173246a6393fb6faa83b388ec3e8cac3ec83
JourneyCodesAyush <mithilesh71170@gmail.com>
2025-08-27 13:48:35 +0530

    feat(gui/split-gui): use last selected folder for PDF splitting if user preference is enabled

--- gui/split_gui.py
+++ gui/split_gui.py
@@ -1,9 +1,11 @@
 # PDF split GUI
 
 import os
+from pathlib import Path
 from tkinter import filedialog, messagebox, simpledialog
 
 from config.config import setup_logger
+from config.preferences_manager import get_preferences, set_preferences
 from core.error_handler import handle_exception
 from core.pdf_splitter import split_pdf
 from gui.error_handler_gui import show_message
@@ -25,8 +27,16 @@ def split_pdf_gui(root) -> None:
 
     logger.info("Split PDF operation started")
     try:
+
+        prefs = get_preferences()
+        initial_directory = Path.home()
+        if prefs.get("save_preferences") and prefs.get("last_split_file"):
+            initial_directory = prefs["last_split_file"]
+            initial_directory = Path(str(initial_directory))
+
         file_path = filedialog.askopenfilename(
             title="Select PDF to split",
+            initialdir=initial_directory,
             filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.pdf")],
             defaultextension="*.pdf",
         )
@@ -69,6 +79,11 @@ def split_pdf_gui(root) -> None:
                     for f in result.data["files"]:
                         logger.info(f"Created: {os.path.join(output_dir,f)}")
                 logger.info(f"Split operation finished with message: {result.message}")
+
+                # Update the preferences if user opted for it
+
+                if prefs.get("save_preferences"):
+                    set_preferences(last_split_file=str(Path(file_path).parent))
             else:
                 logger.warning(
                     f"Split PDF operation returned failure message: {result.message}"
